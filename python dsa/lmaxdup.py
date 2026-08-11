ce4bf916b80cf5ad314b788abacb3df2e2034492
Lohit534 <22h41a0534lohit@gmail.com>
2026-08-11 21:45:59 +0530

    Add implementation for finding the longest substring without repeating characters

---
+++ python dsa/lmaxdup.py
@@ -0,0 +1,16 @@
+#3. Longest Substring Without Repeating Characters
+
+s= "abcabcbb"
+l=0
+ans=0
+uni=set()
+for i in range(len(s)):
+    ch=s[i]
+    if ch not in uni:
+        uni.add(ch)
+    else:
+        while ch in uni:
+            uni.remove(s[l])
+            l+=1
+        uni.add(ch)
+    ans=max(ans,i-l+1)
